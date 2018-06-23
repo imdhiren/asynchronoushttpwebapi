0e01664a7f48f859ad398ba538b0be1a28a4ec23
Dhiren Patel <dhiren@jkbsoftware.com>
2018-06-23 23:41:49 +0530

    Add project files.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [CompressResponse]
+        public IEnumerable<Person> Get()
+        {
+            return new Person[]
+            {
+                new Person()
+                {
+                    Name = "James",
+                    Age = 21
+                },
+                    new Person()
+                {
+                    Name = "Jon",
+                    Age = 21
+                }
+            };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public IEnumerable<Person> Post([FromBody]string value)
+        {
+            return new Person[]
+    {
+                new Person()
+                {
+                    Name = "James",
+                    Age = 21
+                },
+                    new Person()
+                {
+                    Name = "Jon",
+                    Age = 21
+                }
+    };
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using API.Models;
+using APICore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static ApiClient client => new ApiClient(true);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Task.Run(async () =>
+                {
+
+                    var response = await client.GetJsonAsync<IEnumerable<Person>>("http://localhost:36232/Api/Values");
+                    var people = response.BodyDeserialized;
+
+                    var properties = typeof(Person).GetProperties();
+                    var output = String.Join("\r\n", people.Select(x =>
+                                     String.Join(",", properties.Select(f => f.GetValue(x)))
+                                 ));
+
+                    Console.WriteLine(output);
+
+                }).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
